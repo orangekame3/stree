4b22edc7a6738e2a22a14e0c4dc0935b379f7485
orangekame3 <miya.org.0309@gmail.com>
2023-09-13 20:10:12 +0900

    add test

--- pkg/tree.go
+++ pkg/tree.go
@@ -33,24 +33,18 @@ func addNodeWithColor(parent *gtree.Node, keys []string, depth int) *gtree.Node 
 	if len(keys) == 0 {
 		return nil
 	}
-	// Determine the correct color for the node based on its type (file or directory)
-	coloredKey := colorizeKey(keys[0], len(keys) == 1)
-	// Add node to the tree
+
+	// Add a colored node to the tree based on whether the current key represents a file or a directory
+	coloredKey := getColorizedKey(keys[0], len(keys) == 1)
 	node := parent.Add(coloredKey)
+
 	return addNodeWithColor(node, keys[1:], depth+1)
 }
 
-func colorizeKey(key string, isFile bool) string {
+// getColorizedKey returns the colored representation of a key, with different colors for files and directories
+func getColorizedKey(key string, isFile bool) string {
 	if isFile {
-		return colorFile(key)
+		return color.New(color.FgGreen).SprintFunc()(key)
 	}
-	return colorDirectory(key)
-}
-
-func colorFile(key string) string {
-	return color.New(color.FgGreen).SprintFunc()(key)
-}
-
-func colorDirectory(key string) string {
 	return color.New(color.FgBlue).SprintFunc()(key)
 }
